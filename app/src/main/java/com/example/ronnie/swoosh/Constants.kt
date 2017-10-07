ff903ecba4997b9bd7242457986a396de099a7a3
RonnieDipple <Ronniedipple@gmail.com>
2017-10-07 16:46:26 -0500

    lesson 7

---
+++ app/src/main/java/com/example/ronnie/swoosh/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.ronnie.swoosh
+
+/**
+ * Created by ronnie on 07/10/17.
+ */
+
+const val EXTRA_LEAGUE = "league"
--- app/src/main/java/com/example/ronnie/swoosh/SkillActivity.kt
+++ app/src/main/java/com/example/ronnie/swoosh/SkillActivity.kt
@@ -1,12 +1,30 @@
 package com.example.ronnie.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_skill.*
 
-class SkillActivity : AppCompatActivity() {
+class SkillActivity : BaseActivity() {
+    var league = ""
+    var selectedLevel = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
+        league = intent.getStringExtra(EXTRA_LEAGUE)
+        println(league)
+    }
+
+    fun onBeginnerBtnClicked (view: View){
+        ballerBtn.isChecked = false
+        selectedLevel = "beginner"
+
+    }
+
+    fun onBallerBtnClicked (view: View){
+        beginnerBtn.isChecked = false
+        selectedLevel = "baller"
     }
 }
